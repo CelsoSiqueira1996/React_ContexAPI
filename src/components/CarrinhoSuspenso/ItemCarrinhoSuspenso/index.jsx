ec7516486660be79d933e4c1749943d4e502eff4
Celso <celso.jacinto.siqueira.neto@gmail.com>
2024-08-20 17:08:22 -0300

    corrigindo alguns paths

--- src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
+++ src/components/CarrinhoSuspenso/ItemCarrinhoSuspenso/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Quantidade from "@/components/Quantidade";
 import Botao from "@/components/Botao";
 import ValorFormatado from "@/components/ValorFormatado";
-import { useCarrinhoContext } from "@/Hooks/useCarrinhoContext";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
 const ItemCarrinhoSuspenso = ({
   itemCarrinho
